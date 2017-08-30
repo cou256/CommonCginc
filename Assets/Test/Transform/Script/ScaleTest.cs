2f928eb2c564db39c7a0e668323f7a0caf3082bb
Crimson_Apple <@Crimson_Apple>
2017-08-30 17:03:06 +0900

    最初のコミット

---
+++ Assets/Test/Transform/Script/ScaleTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class ScaleTest : TestBase
+{
+    protected override void TestUpdate(float dt, Material mat, Transform cpu, Transform gpu, Vector3 cpuInitPos, Vector3 gpuInitPos)
+    {
+        cpu.transform.localScale = Vector3.one * Mathf.Sin(dt);
+        mat.SetVector("_Scale", cpu.transform.lossyScale);
+    }
+}
---
+++ Assets/Test/Transform/Script/TRSTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TRSTest : TestBase
+{
+    Vector3 rotate;
+    protected override void TestUpdate(float dt, Material mat, Transform cpu, Transform gpu, Vector3 cpuInitPos, Vector3 gpuInitPos)
+    {
+        var translate = Vector3.one * Mathf.Sin(dt);
+        rotate += Vector3.one;
+        cpu.transform.localPosition = cpuInitPos + translate;
+        cpu.transform.rotation = Quaternion.Euler(rotate);
+        cpu.transform.localScale = Vector3.one * Mathf.Sin(dt);
+        mat.SetVector("_Translate", translate);
+        mat.SetVector("_Rotate", rotate);
+        mat.SetVector("_Scale", cpu.transform.lossyScale);
+    }
+}
